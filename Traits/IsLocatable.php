c4ca1d3e9cd5ab104362a733d0a42631355960bc
Cristian Marin <marin101998@gmail.com>
2025-05-06 18:28:17 -0500

    + fix | v10 06-05-25 fix locatable

--- Traits/IsLocatable.php
+++ Traits/IsLocatable.php
@@ -30,9 +30,10 @@ trait IsLocatable
     $cityId = $params['data']['locatable']['city_id'] ?? null;
     $countryId = $params['data']['locatable']['country_id'] ?? null;
     $provinceId = $params['data']['locatable']['province_id'] ?? null;
+    $locatableRepository = app('Modules\Ilocations\Repositories\LocatableRepository');
 
     if ($cityId || $countryId || $provinceId) {
-      \Modules\Ilocations\Entities\Locatable::updateOrCreate([
+      $locatableRepository->updateOrCreate([
         'entity_type' => get_class($this),
         'entity_id' => $this->id,
       ], [
